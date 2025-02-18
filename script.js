d115cb392a2a89000570b5f3adfcf007575520c9
bakha2209 <bakhodir2209@gmail.com>
2025-02-18 18:21:59 +0900

    feat: adding group class

--- script.js
+++ script.js
@@ -5,15 +5,29 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.mo
 //scene 
 const scene = new THREE.Scene()
 
+//Group
+const group=new THREE.Group()
+
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color: "purple"})
 const mesh = new THREE.Mesh(geometry, material)
-mesh.position.z=1;
-mesh.scale.x=1;
-mesh.rotation.x=Math.PI*0.25;
-mesh.rotation.y=Math.PI*1.2
 
-scene.add(mesh)
+mesh.position.z=1;
+
+//MeshTwo
+const geometryT=new THREE.BoxGeometry(1,1,1);
+const materialT=new THREE.MeshBasicMaterial({color:"green"});
+const meshT=new THREE.Mesh(geometryT,materialT);
+meshT.position.y=2;
+
+
+group.add(mesh,meshT);
+group.position.x=1
+scene.add(group);
+
+//axesHelper
+const axesHelper= new THREE.AxesHelper()
+scene.add(axesHelper)
 
 //camera 
 const aspect = {
